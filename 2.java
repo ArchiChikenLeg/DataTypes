b9d2f77028f8e57e2db553ee765ebe0af1b89532
ArchiChikenLeg <andrienkolidia@gmail.com>
2022-03-10 13:14:17 +0200

    Point class used in tasks

---
+++ 2.java
@@ -0,0 +1,27 @@
+package DataTypes;
+
+import java.util.Scanner;
+
+public class DataTypes {
+	
+	
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		Point[] arr = new Point [n];
+		
+		for(int i = 0; i < n; i++)
+			arr[i] = new Point(in.nextInt(), in.nextInt());
+
+		double cX = 0, cY = 0;
+		for(int i = 0; i < n; i++) {
+			cX += arr[i].X();
+			cY += arr[i].Y();
+		}
+		
+		System.out.println(cX/(double)n + " " + cY/(double)n);
+		in.close();
+	}
+}
+
+
---
+++ Point.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class Point {
+	public int x;
+	public int y;
+	Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	int X() { return x; }
+	int Y() { return y; }
+	public void setX(int x) {this.x = x;}
+	public void setY(int y) {this.y = y;}
+
+}
